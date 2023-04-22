bf6988279be8c7177c7f3a5563cad73290d42cae
ruvesh <ruvesh2145@gmail.com>
2023-04-23 03:23:14 +0530

    Completed about, skills and footer implementation. Added javascript for dynamically active navbar links. Added higher quality image.

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,24 @@
-alert('This website is still under development. Some of the links may not work yet.');
+window.onload = () => {
+    navLinks = document.querySelectorAll('header nav a');
+    sections = document.querySelectorAll('section');
+};
+
+window.onscroll = () => {
+    console.log('scrolling');
+    console.log(sections);
+    console.log(navLinks);
+    sections.forEach(section => {
+        let top = window.scrollY;
+        let offset = section.offsetTop - 150;
+        let height = section.offsetHeight;
+        let id = section.getAttribute('id');
+        
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+};
